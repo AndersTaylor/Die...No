32a15ff4dbde86392dc13988fc8fc6fa4f79cb23
Anders Taylor <111925828+AndersTaylor@users.noreply.github.com>
2024-02-16 12:19:09 +0100

    import awesome assets (sorry big commit)

---
+++ Die... No!/Dinos 2D/Assets/Scripts/Parallax.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    //AT attach this script to your empty parent object storing the different sprites on different layers
+    // (i.e. 3 tiled sprites for the fore, mid, and backgrounds)
+    
+    private float length;
+    private SpriteRenderer[] childSprites;
+    private float startPos;
+
+    public GameObject camera;
+    public float parallaxEffect; //AT set to a number between 0 and 1. 1 = no parallax. >1 makes the backgrounds move opposite direction
+
+    void Start()
+    {
+        startPos = transform.position.x;
+        childSprites = GetComponentsInChildren<SpriteRenderer>(); //AT get all the sprites in the children of the attached empty game object
+    }
+
+    void Update()
+    {
+        SetSpritesPos();
+    }
+
+    void SetSpritesPos()
+    {
+        for (int i = 0; i < childSprites.Length; i++)
+        {
+            length = childSprites[i].bounds.size.x;
+            
+            float temp = (camera.transform.position.x * (1 - parallaxEffect));
+            float dist = (camera.transform.position.x * parallaxEffect);
+
+            transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+
+            if (temp > startPos + length)
+                startPos += length;
+            else if (temp < startPos - length)
+                startPos -= length;
+        }
+    }
+}
