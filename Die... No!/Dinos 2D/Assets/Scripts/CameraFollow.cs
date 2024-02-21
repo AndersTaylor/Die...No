84397b52c3c5b7e2b64b051cf2633679e11fa95a
Anders Taylor <111925828+AndersTaylor@users.noreply.github.com>
2024-02-21 13:37:24 +0100

    Camera Follow

--- Die... No!/Dinos 2D/Assets/Scripts/CameraFollow.cs
+++ Die... No!/Dinos 2D/Assets/Scripts/CameraFollow.cs
@@ -2,10 +2,11 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vector2 = System.Numerics.Vector2;
 
 public class CameraFollow : MonoBehaviour
 {
-    public float followSpeed = 1.0f;
+    public float followSpeed = 9f;
     public float deadZone = 0.25f;
     public float topEdge;
     public float bottEdge;
@@ -13,25 +14,45 @@ public class CameraFollow : MonoBehaviour
     public float leftEdge;
 
     private GameObject player;
+    private PlayerController pController;
     private Vector3 groundedTarget;
     private Vector3 airTarget;
+    private float _followSpeed;
     
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        pController = player.GetComponent<PlayerController>();
     }
 
     private void Update()
-    { 
+    {
+        Debug.Log(Vector2.Distance(new Vector2(player.transform.position.x, player.transform.position.y), new Vector2(transform.position.x, transform.position.y)));
+        //AT Set the _followSpeed (the actual value the logic checks) depending on how far the camera is from player
+        if (Input.GetAxisRaw("Horizontal") == 0 && pController.isGrounded)
+        {
+            _followSpeed = 20;
+        }
+        else if (Vector2.Distance(new Vector2(player.transform.position.x, player.transform.position.y),
+                new Vector2(transform.position.x, transform.position.y)) > 3)
+        {
+            _followSpeed = pController.speed;
+        }
+        else
+        {
+            _followSpeed = followSpeed;
+        }
+        
         groundedTarget = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
         airTarget = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
 
-        //if (transform.position.x > leftEdge && transform.position.x < rightEdge && transform.position.y < topEdge && transform.position.y > bottEdge) {
-        if (player.GetComponent<PlayerController>().isGrounded && Vector3.Distance(transform.position, groundedTarget) > deadZone)
+        if (pController.isGrounded && Vector3.Distance(transform.position, groundedTarget) > deadZone)
         {
-                // Move our position a step closer to the target.
-            float step = followSpeed * Time.deltaTime; // calculate distance to move, change by frame rate
+            // Move our position a step closer to the target.
+            float step = _followSpeed * Time.deltaTime; // calculate distance to move, change by frame rate
             Vector3 newPosition = transform.position;
+            
+            // Move the camera within edges
             if (groundedTarget.x > leftEdge && groundedTarget.x < rightEdge)
             {
                 newPosition.x = Mathf.MoveTowards(transform.position.x, groundedTarget.x, step);
@@ -41,13 +62,11 @@ public class CameraFollow : MonoBehaviour
                 newPosition.y = Mathf.MoveTowards(transform.position.y, groundedTarget.y, step);
             }
             transform.position = newPosition;
-
-            //transform.position = Vector3.MoveTowards(transform.position, groundedTarget, step);
         }
-        else if (!player.GetComponent<PlayerController>().isGrounded && Vector3.Distance(transform.position, airTarget) > deadZone) 
+        else if (!pController.isGrounded && Vector3.Distance(transform.position, airTarget) > deadZone) 
         {
             // Move our position a step closer to the target.
-            float step = followSpeed * Time.deltaTime;
+            float step = _followSpeed * Time.deltaTime;
             Vector3 newPosition = transform.position;
             if (airTarget.x > leftEdge && airTarget.x < rightEdge)
             {
@@ -59,6 +78,5 @@ public class CameraFollow : MonoBehaviour
             }
             transform.position = newPosition;
         }
-    
     }
 }
