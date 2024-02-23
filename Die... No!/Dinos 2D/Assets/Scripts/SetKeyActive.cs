c5aa022cd716ee679f54f80c2ab687d94b2c0d9a
ssd74 <ssd74@cornell.edu>
2024-02-23 17:39:00 +0100

    key ;-;

--- Die... No!/Dinos 2D/Assets/Scripts/SetKeyActive.cs
+++ Die... No!/Dinos 2D/Assets/Scripts/SetKeyActive.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class SetKeyActive : MonoBehaviour
 {
+    private bool guyDiscovered = false;
+    private GameObject character;
+    private SpriteRenderer sr;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        character = GameObject.FindWithTag("Player");
+        sr = GetComponent<SpriteRenderer>();
+        sr.enabled = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!guyDiscovered && character.transform.position.y >= 5.057f && character.transform.position.x >= 43f)
+        {
+            guyDiscovered = true;
+            sr.enabled = true;
+        }
+
     }
 }
