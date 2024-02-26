bf24ee28bf8d806c07337a38535c1b628b107816
ssd74 <ssd74@cornell.edu>
2024-02-26 14:20:12 +0100

    dino teleport and sit/stop

--- Die... No!/Dinos 2D/Assets/Scripts/DinoFollower.cs
+++ Die... No!/Dinos 2D/Assets/Scripts/DinoFollower.cs
@@ -7,12 +7,12 @@ using UnityEngine.Serialization;
 public class DinoFollower : MonoBehaviour
 {
     public float speed = 1;
-    
-    private bool isSitting;
+
+    private bool isSitting = false;
     private GameObject player;
     private Transform target;
     private Rigidbody2D rb;
-    
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
@@ -24,15 +24,48 @@ public class DinoFollower : MonoBehaviour
     // - Smoothly follow player on x axis, always trying to get to its spot in line behind player
     //    - What if player turns around?
     // - Sit on player keypress to stop moving
-    
 
-    void Update () 
+
+    void Update()
     {
-        if (Vector2.Distance(target.position, transform.position) < 2)
+        if (!isSitting)
+        {
+            if (Vector2.Distance(target.position, transform.position) < 2)
+                rb.velocity = Vector2.zero;
+            else if (target.position.x - 2 > transform.position.x)
+                rb.velocity = new Vector2(speed, 0);
+            else if (target.position.x - 2 < transform.position.x)
+                rb.velocity = new Vector2(-speed, 0);
+
+            //teleport
+            if (Vector2.Distance(target.position, transform.position) > 10)
+            {
+                transform.position = target.position;
+            }
+
+        }
+
+        //sit when clicked on
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
+
+            if (hit.collider.gameObject == gameObject)
+            {
+                StopStart();
+            }
+        }
+    }
+
+    void StopStart()
+    {
+        isSitting = !isSitting;
+
+        if (isSitting)
+        {
             rb.velocity = Vector2.zero;
-        else if(target.position.x - 2 > transform.position.x)
-            rb.velocity = new Vector2(speed, 0);
-        else if(target.position.x - 2 < transform.position.x)
-            rb.velocity = new Vector2(-speed, 0);
+        }
     }
 }
+
