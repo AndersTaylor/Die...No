e3dcc8bb99d5bb1ef109e1edf16378b10da1aa6d
ssd74 <ssd74@cornell.edu>
2024-02-28 11:23:57 +0100

    multiple respawn lamps functionality

--- Die... No!/Dinos 2D/Assets/Scripts/PlayerDeath.cs
+++ Die... No!/Dinos 2D/Assets/Scripts/PlayerDeath.cs
@@ -13,11 +13,7 @@ public class PlayerDeath : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameObject respawnObject = GameObject.FindGameObjectWithTag("RespawnLamp");
-        if (respawnObject != null)
-        {
-            respawnPosition = respawnObject.transform.position;
-        }
+        respawnPosition = new Vector3(-12.62f, -0.21f, 0);
     }
 
     // Update is called once per frame
@@ -33,6 +29,14 @@ public class PlayerDeath : MonoBehaviour
         
         GameObject newPlayer = Instantiate(gameObject, respawnPosition, Quaternion.identity);
         Destroy(gameObject);
+    }
+
+    public void UpdateLastCheckpoint(Vector3 checkpointPosition)
+    {
+        if (checkpointPosition.x > respawnPosition.x)
+        {
+            respawnPosition = checkpointPosition;
+        }
     }
 
 }
