2fbf5e96f8cd59450268dbae874d86343663be54
friedmam <friedmam@whitman.edu>
2024-03-01 13:28:18 +0100

    ?

--- Die... No!/Dinos 2D/Assets/Scripts/KeyFollow.cs
+++ Die... No!/Dinos 2D/Assets/Scripts/KeyFollow.cs
@@ -8,68 +8,28 @@ public class KeyFollow : MonoBehaviour
 {
     public float speed = 1;
 
-    private Animator animator;
-    private GameObject player;
-    private GameObject corruptDino;
-    private GameController gc;
-    private Transform target;
-    private Rigidbody2D rb;
-    private bool guyDiscovered = false;
-    private SpriteRenderer sr;
-    private bool shouldMove;
-    private bool isSelected;
+    private Animator animator; 
+    private PickUp pickUpScript;
+    private SpriteRenderer sr; 
 
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
-        corruptDino = GameObject.FindGameObjectWithTag("Corrupted");
+        Transform parentTransform = transform.parent;
+
+        if (parentTransform != null)
+        {
+            pickUpScript = parentTransform.GetComponent<PickUp>();
+        }
         animator = GetComponent<Animator>();
-        target = player.transform;
-        rb = GetComponent<Rigidbody2D>();
-        shouldMove = false;
-        isSelected = false;
         sr = GetComponent<SpriteRenderer>();
         sr.enabled = false;
-        gc = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
     }
 
     void Update()
     {
-        if (!player)
+        if (!pickUpScript.guyDiscovered && pickUpScript.player.transform.position.y >= 5.057f && pickUpScript.transform.position.x >= 43f)
         {
-            player = GameObject.FindGameObjectWithTag("Player");
-            target = player.transform;
-        }
-
-        if (!guyDiscovered && player.transform.position.y >= 5.057f && player.transform.position.x >= 43f)
-        {
-            guyDiscovered = true;
             sr.enabled = true;
-        }
-
-        if (shouldMove)
-        {
-            Debug.Log("Move!");
-            if (target.position.x > transform.position.x)
-            {
-                rb.velocity = new Vector2(speed, rb.velocity.y);
-            }
-            else if (target.position.x < transform.position.x)
-            {
-                rb.velocity = new Vector2(-speed, rb.velocity.y);
-            }
-            if (target.position.y > transform.position.y)
-            {
-                rb.velocity = new Vector2(rb.velocity.x, speed);
-            }
-            else if (target.position.y < transform.position.y)
-            {
-                rb.velocity = new Vector2(rb.velocity.x, -speed);
-            }
-        }
-        else
-        {
-            rb.velocity = new Vector2(0, rb.velocity.y);
         }
     }
 
@@ -77,33 +37,16 @@ public class KeyFollow : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            if (isSelected) 
+            if (!pickUpScript.isSelected)
             {
-                shouldMove = false;
-                Debug.Log("Fox is in range");
-            } 
-            else 
-            {
-                shouldMove = true;
                 animator.SetBool("Selected", true);
-                isSelected = true;
             }
         }
         if (other.gameObject.CompareTag("Corrupted"))
         {
-            target = corruptDino.transform;
             animator.SetTrigger("Unlock");
             yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
             Destroy(gameObject);
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            shouldMove = true;
-            Debug.Log("Fox is out of range");
         }
     }
 }
--- Die... No!/Dinos 2D/Assets/Scripts/PickUp.cs
+++ Die... No!/Dinos 2D/Assets/Scripts/PickUp.cs
@@ -5,16 +5,93 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
-    //AT Goes on every key. Sends the value to the GameController to distribute the value to scene listeners.
+
+    public float speed = 1;
+    public GameObject player;
+    private GameObject corruptDino;
+    private GameController gc;
+    private Transform target;
+    private Rigidbody2D rb;
+    public bool guyDiscovered = false;
+    private bool shouldMove;
+    public bool isSelected;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        corruptDino = GameObject.FindGameObjectWithTag("Corrupted");
+        target = player.transform;
+        rb = GetComponent<Rigidbody2D>();
+        shouldMove = false;
+        isSelected = false;
+        gc = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+    }
+
+    void Update()
+    {
+        if (!player)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            target = player.transform;
+        }
+
+        if (!guyDiscovered && player.transform.position.y >= 5.057f && player.transform.position.x >= 43f)
+        {
+            guyDiscovered = true;
+        }
+
+        if (shouldMove)
+        {
+            Debug.Log("Move!");
+            if (target.position.x > transform.position.x)
+            {
+                rb.velocity = new Vector2(speed, rb.velocity.y);
+            }
+            else if (target.position.x < transform.position.x)
+            {
+                rb.velocity = new Vector2(-speed, rb.velocity.y);
+            }
+            if (target.position.y > transform.position.y)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, speed);
+            }
+            else if (target.position.y < transform.position.y)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, -speed);
+            }
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>().pickedUpKey = true;
+            if (isSelected) 
+            {
+                shouldMove = false;
+            } 
+            else 
+            {
+                shouldMove = true;
+                isSelected = true;
+            }
         }
         if (other.gameObject.CompareTag("Corrupted"))
         {
-            GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>().unlockCorrupted = true;
+            gc.unlockCorrupted = true;
+            target = corruptDino.transform;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            shouldMove = true;
         }
     }
 }
