38bfafe6d82387cbc68a88c976640548da37f1ec
Anders Taylor <111925828+AndersTaylor@users.noreply.github.com>
2024-03-02 16:50:40 +0100

    Pause Menu & Scene loader

--- Die... No!/Dinos 2D/Assets/Scripts/Menu.cs
+++ Die... No!/Dinos 2D/Assets/Scripts/Menu.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class mene : MonoBehaviour
-{
-    // Start is called before the first frame update
-   public void LoadScene(string scene)
+public class Menu : MonoBehaviour
+{ 
+    public void LoadScene()
+    { 
+        GameObject gc = GameObject.FindGameObjectWithTag("GameController"); 
+        gc.GetComponent<GameController>().StartCoroutine(nameof(GameController.LoadNextScene)); 
+    }
+
+    public static void LoadSceneName(string scene)
     {
-        SceneManager.LoadScene(scene);
+        GameObject gc = GameObject.FindGameObjectWithTag("GameController");
+        gc.GetComponent<GameController>().sceneName = scene;
+        gc.GetComponent<GameController>().StartCoroutine(nameof(GameController.LoadSceneName)); 
     }
 }
