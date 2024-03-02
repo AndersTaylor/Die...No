74616e5087b5528a1df5cb5c90daa4e775a5a824
Anders Taylor <111925828+AndersTaylor@users.noreply.github.com>
2024-03-02 22:28:18 +0100

    key follow

--- Die... No!/Dinos 2D/Assets/Scripts/PickUp.cs
+++ Die... No!/Dinos 2D/Assets/Scripts/PickUp.cs
@@ -1,17 +1,75 @@
-using System;
+ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PickUp : MonoBehaviour
 {
-    //AT Goes on every key. Sends the value to the GameController to distribute the value to scene listeners.
+    public float damp = 1;
+    
+    private GameObject player;
+    private Transform target;
+    private bool shouldMove;
+    private GameController gc;
+    
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        //gc = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        target = player.transform;
+        shouldMove = false;
+    }
+
+    Vector3 vel = Vector3.zero;
+    private void Update()
+    {
+        if (shouldMove)
+            transform.position = Vector3.SmoothDamp(transform.position, target.position, ref vel, damp);
+    }
+
+    /*void FixedUpdate()
+    {
+        if (!player)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            target = player.transform;
+        }
+       
+        float distance = Vector3.Distance(transform.position, target.position);
+        float tempSpeed = speed * Mathf.Sqrt(distance);
+        //shouldMove = distance > 1;
+        
+        
+        if (shouldMove)
+        {
+            if (target.position.x - transform.position.x > 1)
+            {
+                rb.velocity = new Vector2(tempSpeed, rb.velocity.y);
+            }
+            else if (target.position.x - transform.position.x < 1)
+            {
+                rb.velocity = new Vector2(-tempSpeed, rb.velocity.y);
+            }
+            if (target.position.y - transform.position.y > 1)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, tempSpeed);
+            }
+            else if (target.position.y - transform.position.y < 1)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, -tempSpeed);
+            }
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, 0);
+        }
+    } */
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>().pickedUpKey = true;
-            gameObject.SetActive(false);
-        }
+        if (!other.gameObject.CompareTag("Player")) return;
+        //gc.pickedUpKey = true;
+        shouldMove = true;
     }
 }
